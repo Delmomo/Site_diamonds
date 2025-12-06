a885f7ee7e30714cf5586fc4afbb997810a9a1f1
Drain Delphine <delphinedrain@hotmail.fr>
2025-12-06 15:49:46 +0100

    Ajout du thème, du titre et de la question pour afficher le résultat en rose

--- Visualisation_diamonds/app.R
+++ Visualisation_diamonds/app.R
@@ -1,11 +1,5 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+
+# Imporation des packages ----
 
 library(shiny)
 library(dplyr)
@@ -16,43 +10,63 @@ library(bslib)
 library(thematic)
 library(plotly)
 
-# Define UI for application that draws a histogram
+# Visualisation de la Data ----
+
+data("diamonds")
+
+
+# UI ----
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  theme = bs_theme(version = 5, bootswatch = "minty"),
+  
+  # Application title
+  titlePanel("Exploration de la BDD Diamonds"),
+  
+  # Sidebar with inputs
+  sidebarLayout(
+    sidebarPanel(
+      # Ajout du Bouton_rose
+      radioButtons(inputId = "Bouton_rose",
+                   label = "Colorier en rose ?",
+                   choices = c("Oui", "Non"),
+                   selected = "Non"),
+      
+      # Le slider original
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  output$distPlot <- renderPlot({
+    
+    # Détermine la couleur des barres basée sur le Bouton_rose
+    couleur_barres <- ifelse(input$Bouton_rose == "Oui", "deeppink", "darkgray")
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, 
+         col = couleur_barres, # Utilisation de la variable de couleur
+         border = 'white',
+         xlab = 'Waiting time to next eruption (in mins)',
+         main = 'Histogram of waiting times')
+  })
 }
 
-# Run the application 
+# Lancer l'Application Web ----
+
 shinyApp(ui = ui, server = server)
